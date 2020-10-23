43721c2681e3ca355038607d662a651d9153d67f
mohamadvatandoost512 <mohamadvatandoost512@gmail.com>
2020-10-23 02:25:26 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"errors"
+
+	"github.com/sirupsen/logrus"
+	cli "github.com/virtual-kubelet/node-cli"
+	logruscli "github.com/virtual-kubelet/node-cli/logrus"
+	"github.com/virtual-kubelet/node-cli/provider"
+	"github.com/virtual-kubelet/virtual-kubelet/log"
+	logruslogger "github.com/virtual-kubelet/virtual-kubelet/log/logrus"
+)
+
+func main() {
+	ctx := cli.ContextWithCancelOnSignal(context.Background())
+	logger := logrus.StandardLogger()
+
+	log.L = logruslogger.FromLogrus(logrus.NewEntry(logger))
+	logConfig := &logruscli.Config{LogLevel: "info"}
+
+	node, err := cli.New(
+		cli.WithProvider("demo", func(cfg provider.InitConfig) (provider.Provider, error) {
+			return nil, errors.New("your implementation goes here")
+		}),
+		// Adds flags and parsing for using logrus as the configured logger
+		cli.WithPersistentFlags(logConfig.FlagSet()),
+		cli.WithPersistentPreRunCallback(func() error {
+			return logruscli.Configure(logConfig, logger)
+		}),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	// Args can be specified here, or os.Args[1:] will be used.
+	if err := node.Run(ctx); err != nil {
+		panic(err)
+	}
+}
